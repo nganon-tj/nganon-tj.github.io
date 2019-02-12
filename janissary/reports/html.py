c8839a7bae0692326a482f87f82a336f310d58bf
Jeff McBride <mcbridejc@gmail.com>
2019-02-12 09:59:54 -0800

    Add comments in html.py

--- janissary/reports/html.py
+++ janissary/reports/html.py
@@ -5,11 +5,22 @@ import os
 from .report import report
 
 @jinja2.contextfunction                                                                                                                                                                                         
-def include_file(ctx, name):                                                                                                                                                                                   
-    env = ctx.environment                                                                                                                                                                                      
+def include_file(ctx, name):
+    """Create a jinja2 function for including a file without parsing it as a template
+
+    The standard jinja2 `include` function will attempt to parse the included 
+    file as a template, and javascript (not enclosed in a <script> tag) is not a valid
+    jinja2 template. 
+    """
+    env = ctx.environment
     return jinja2.Markup(env.loader.get_source(env, name)[0])  
 
 def render_html(header_dict, timestamped_commands):
+    """Returns HTML output for report file
+
+    header_dict - A dict containing the information parsed from the header of the log file
+    timestamped_commands - List of TimestampedCommand objects parsed from the body of the log file
+    """
     report_data = report(header_dict, timestamped_commands)
 
     fileDir = os.path.dirname(os.path.realpath(__file__))
